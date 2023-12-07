39abd63370b6e27496f4b09dc0e979a5660d9929
Munib78 <munibbakrawala@gmail.com>
2023-12-07 11:08:52 +0530

    First commit

--- index.js
+++ index.js
@@ -1,34 +1,104 @@
-
-const express = require("express");
 const mongoose = require("mongoose");
+const express = require("express");
 const bodyparser = require("body-parser");
-const MongoClient = require('mongodb').MongoClient
 
-
-
+mongoose.connect("mongodb://localhost:27017/hospital_appointment",{useNewUrlParser : true});
 const app = express ();
-app.use(bodyparser.urlencoded({extended:false}));
-
+app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
-mongoose.connect("mongodb/localhost:27017/");
+app.use(express.json());
+
+const patient_schema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    address:{
+        type: String,
+        required:true
+    },
+    mobile:{
+        type:Number,
+        required: true
+    },
+    email : {
+        type:String,
+        unique:[true,"email is already taken"],
+        required: true
+        
+    },
+   
+});
+
+const patient = mongoose.model('Patient',patient_schema);
 
 
+const doctor_schema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
 
-// app.get("/",function(req,res){ res.send("<h1> Hello World </h1>")})
-// app.get("/home", function(req, res) {
-//     res.send(__dirname+ "home.html");
-// })
-// app.get("/about", function(req, res) {
-//     res.send(__dirname+ "home.html");
-// })
-// app.get("/contact", function(req, res) {
-//     res.send(__dirname+ "home.html");
-// })
-// app.get("/ngo/:name", function(req, res) {
-//     res.send(__dirname+ "ngopage.html");
-// })
-// app.get("/news", function(req,res) {
-//     res.send(__dirname+"news.html");
-// })
+    department:{
+        type: String,
+        required: true
+    },
 
-// app.listen(3000,()=> {console.log("Server started at port 3000")});
+    experience:{
+        type: String,
+        required: true
+    },
+
+    degree:{
+        type: String,
+        required: true
+    },
+
+    fees:{
+        type: Number,
+        required: true
+    },
+
+    service:{
+        type: String,
+        //required: true
+    },
+
+    time_slot:{
+        type: Date,
+       // required: true
+    }
+});
+
+const doctor = mongoose.model('Doctor',doctor_schema)
+
+const appointment_schema = new mongoose.Schema({
+    patient_id:{type: mongoose.Schema.Types.ObjectId,ref:"Patient"},
+    
+    doctor_id: {type: mongoose.Schema.Types.ObjectId,ref:"Doctor"},
+
+    slot: {
+        type: Date,
+        requred: true
+    },
+
+    date:{
+        type: Date,
+        required: true
+    },
+
+    status: {
+        type: String,
+        requred: true
+    }
+});
+
+const appointment = mongoose.model("Appointment",appointment_schema);
+
+app.listen(3000);
+
+
